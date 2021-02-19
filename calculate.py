0b4ddfe44382ecedbf5003425033c30b24dad065
gao ming <mingming.gao@eeoa.com>
2021-02-19 15:15:21 +0800

    确保每个进程执行结束

--- calculate.py
+++ calculate.py
@@ -2,15 +2,18 @@
 # coding: utf-8 
 # Gao Ming Ming Create At 2021-02-19 14:22:03
 # Description:测试multiprocessing.Manager()的变量是否是线程安全的
-# 结论: 不安全,加锁后每次结果都不同,郁闷
+# 结论: 不安全,加锁后每次结果都不同,郁闷,原因是因为有的进程没有执行结束
 import time
 import multiprocessing
 import threading
 
 def worker(lock,data):
     lock.acquire()
-    data["value"] += 1
-    lock.release()
+    try:
+        data["value"] += 1
+    finally:
+        pass
+        lock.release()
 
 if __name__ == '__main__':
     mgr = multiprocessing.Manager()
@@ -19,11 +22,15 @@ if __name__ == '__main__':
     
     lock = multiprocessing.Lock()
 
-    processing_count = 300
+    processing_count = 1000
     jobs = [multiprocessing.Process(target = worker,args = (lock,data)) for i in range(processing_count)]
 
     for job in jobs:
         job.start()
+    
+    # 确保每个进程都执行结束 
+    time.sleep(2)
+
     
     print(data["value"])
 
